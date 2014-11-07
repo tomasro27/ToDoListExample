0e15d74f214674f79d846c1f68a8acac847ef850
tomasro27 <tomasrodriguez@utexas.edu>
2014-11-06 18:37:32 -0600

    listView with simple arrayAdapter and Add button

--- app/src/main/java/com/rodrigueztomas/todolistexample/MainActivity.java
+++ app/src/main/java/com/rodrigueztomas/todolistexample/MainActivity.java
@@ -4,14 +4,33 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends Activity {
+
+    private ListView listView;
+    private ArrayList<String> objects;
+    private ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listview);
+
+        objects = new ArrayList<String>();
+        objects.add("Test 1");
+
+        adapter = new ArrayAdapter<String>(this, R.layout.simple_list_item, objects);
+
+        listView.setAdapter(adapter);
+
     }
 
 
@@ -28,7 +47,10 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.add_item) {
+            //TODO: add item to listview
+            objects.add("Item " + (objects.size() + 1));
+            adapter.notifyDataSetChanged();
             return true;
         }
         return super.onOptionsItemSelected(item);
